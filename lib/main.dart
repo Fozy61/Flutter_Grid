fdf988000ad305ef46869864d1bfaa9629c4e22c
Futsum Estifanos <fozyle@yahoo.de>
2022-06-21 11:03:36 +0200

    erste commit von Grid

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final List<Image> myImage = [
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    const Image(
+        image: AssetImage("assets/imeges/bedroom.png")
+    ),
+    ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GridView Demo"),
+      ),
+    body: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 20,
+        padding: const EdgeInsets.all(8.0),
+        children: myImage,
+        ),
+    );
+  }
+}
+
